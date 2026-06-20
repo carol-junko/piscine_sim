839cf1b6b17002a7a88ccf68ea2ad7a1bfb74e32
deni-b-gomes <carol.junko@gmail.com>
2026-06-20 14:11:37 +0200

    update ft_strlowcase function and add ft_strupcase function

--- ft_strupcase.c
+++ ft_strupcase.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
 #include "helpers.h"
 
-char	*ft_strlowcase(char	*str)
+char	*ft_strupcase(char	*str)
 {
 	int		i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 65 && str[i] <= 90)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] + 32;
+			str[i] = str[i] - 32;
 		}
 		i = i + 1;
 	}
@@ -22,8 +22,8 @@ int	main(void)
 {
 	char	name[20];
 
-	fill_string(name, "CAROLINA");
-	ft_strlowcase(name);
+	fill_string(name, "carOlinA");
+	ft_strupcase(name);
 	write(1, name, 8);
 	write(1, "\n", 1);
 	return (0);
